93a13ab2d826ce9a9a1a9569e216fe05bea915e1
Crispin Wellington <retrogradeorbit@gmail.com>
2017-01-20 21:28:25 +0800

    first cut

--- src/ggj17/core.cljs
+++ src/ggj17/core.cljs
@@ -1,5 +1,9 @@
 (ns ggj17.core
-  (:require ))
+  (:require [infinitelives.pixi.canvas :as c]
+            [infinitelives.pixi.resources :as r]
+            [infinitelives.pixi.texture :as t])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [infinitelives.pixi.macros :as m]))
 
 (enable-console-print!)
 
@@ -13,4 +17,22 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
+
+(defonce bg-colour 0x5D0711)
+
+(defonce canvas (c/init {:layers [:bg :tilemap :ui]
+           :background bg-colour
+           :expand true}))
+
+(defonce main
+  (go
+    ; load resource url with tile sheet
+    (<! (r/load-resources canvas :ui ["img/spritesheet.png"]))
+    
+    #_ (t/load-sprite-sheet!
+     (r/get-texture :notlink :nearest)
+     hero)
+    
+    ))
+
