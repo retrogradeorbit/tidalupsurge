cbc05fb14a602154d544731459e95a6ea25c6b12
Prodge <prodge@prodge.net>
2017-01-21 15:21:12 +0800

    broken words up

--- src/ggj17/assets.cljs
+++ src/ggj17/assets.cljs
@@ -3,4 +3,6 @@
 (def sprites
   {:boat {:pos [0 0] :size [48 32]}
    :title-text {:pos [0 32] :size [256 32]}
+   :tidal {:pos [0 32] :size [100 32]}
+   :upsurge {:pos [100 32] :size [156 32]}
    :cloud {:pos [48 0] :size [48 32]}})
