2ed1355de6cddb72146746e421ba26ed0f44cfa4
Crispin Wellington <retrogradeorbit@gmail.com>
2017-01-22 13:57:58 +0800

    fix some merge break

--- src/ggj17/wave.cljs
+++ src/ggj17/wave.cljs
@@ -128,21 +128,30 @@ void main()
   (set-shader-uniforms shader amp freq phase))
 
 
-(defn update-wave [shader fnum level-x amp freq phase]
+(defn update-colours [shader sky-hue sea-hue]
+  (set! (.-uniforms.skyHue.value shader) sky-hue)
+  (set! (.-uniforms.seaHue.value shader) sea-hue) )
+
+(defn update-wave [shader fnum level-x amp freq phase sky-hue sea-hue]
   (update-background shader fnum amp freq (+ level-x phase)
                      (.-innerWidth js/window)
                      (.-innerHeight js/window))
+  (update-colours shader sky-hue sea-hue)
   (swap! state/state
          #(-> %
               (assoc-in [:wave :fnum] fnum)
+              (assoc-in [:colours :sky-hue] (+ (state/get-sky-hue) 0.0001))
+              (assoc-in [:colours :sea-hue] (+ (state/get-sea-hue) 0.0001))
                                         ;(assoc-in [:level-x] (/ fnum 15))
               )))
 
 (defn wave-update-thread [shader]
   (go
     (loop [fnum 0]
-      (let [{:keys [level-x wave]} @state/state
-            {:keys [amp freq phase]} wave]
-        (update-wave shader fnum level-x amp freq phase)
+      (let [{:keys [level-x wave colours]} @state/state
+            {:keys [amp freq phase]} wave
+            {:keys [sky-hue sea-hue]} colours
+            ]
+        (update-wave shader fnum level-x amp freq phase sky-hue sea-hue)
         (<! (e/next-frame))
         (recur (inc fnum))))))
